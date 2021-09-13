7d9f9fbcb7bb44fc2a5c4929f495f5eec69ed585
wsoh <wsoh@foodtechkorea.com>
2021-09-14 08:56:13 +0900

    #8. useState

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
+import { useState } from 'react';
 import './App.css';
-
-// 0.React 엔진 - 데이터변경감지에서 UI그려주는
-// 1.실행 과정 (index.html) - SPA
-// 2.JSX 문법
-// 3.바벨(자바스크립트 ES5) -> ES6
-
-// (1) return시에 하나의 Dom만 리턴할 수 있다.
-// (2) 변수 선언은 let 혹은 const로만 해야함.
-// (3) if 사용 불가능 -> 삼항연산자.
-// (4) 조건부 렌더링.
-// (5) css 디자인.
-// - 내부에 적는방법
-// - 외부파일에 적는방법
-// - 라이브러리 사용 (부트스트랩, coomponet-styled) 
-
-let a = 10; // 변수
-const b = 20; // 상수
-
-const mystyle = {
-  color: 'red',
-};
+import Sub from './Sub';
 
 function App() {
-  let list = [1,2,3];
+  // let number = 1;
+  const [number, setNumber] = useState(1); // React안에 hooks 라이브러리
+
+  const add = () => {
+    setNumber(number + 1); // 리엑트한테 number 값 변경함을 요청
+    console.log('add', number);
+  };
+
+  // 랜더링 시점 = 상태값 변경
   return (
     <div>
-      <div style={mystyle}>안녕 {a === 10 ? '10입니다.' : '10이 아님'}</div>
-      <h1 className="box-style">해딩태그 {b === 20 && '20입니다.'}</h1>
-      <div>{list.map((n) => <h1>{n}</h1>)}</div>
-      <div>{list.map((n) => n)}</div>
+      <h1>숫자 : {number}</h1>
+      <button onClick={add}>더하기</button>
+      <Sub />
     </div>
   );
 }
---
+++ src/Sub.js
@@ -0,0 +1,15 @@
+// rsc
+import React from 'react';
+
+// 다시 그리는것에 대한 함수 제공 !!!
+// 부모가 변경되면 자식도 재빌드
+
+const Sub = () => {
+  return (
+    <div>
+      <h1>Sub입니다.</h1>
+    </div>
+  );
+};
+
+export default Sub;
