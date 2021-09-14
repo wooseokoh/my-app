6d8abec8d80c2fad80d0647f6392b7a790795acd
wsoh <wsoh@foodtechkorea.com>
2021-09-15 08:42:21 +0900

    #9. useState 완료

--- src/App.js
+++ src/App.js
@@ -3,20 +3,36 @@ import './App.css';
 import Sub from './Sub';
 
 function App() {
-  // let number = 1;
-  const [number, setNumber] = useState(1); // React안에 hooks 라이브러리
+  console.log('App 실행됨');
 
-  const add = () => {
-    setNumber(number + 1); // 리엑트한테 number 값 변경함을 요청
-    console.log('add', number);
+  const [num, setNum] = useState(5);
+
+  let sample = [
+    { id: 1, name: '홍길동' },
+    { id: 2, name: '임꺽정' },
+    { id: 3, name: '코스' },
+    { id: 4, name: '장보고' },
+  ];
+
+  // 다운로드 받음
+  // React안에 hooks 라이브러리
+  const [users, setUsers] = useState(sample); // 레퍼런스 변경되야 동작!!
+
+  const download = () => {
+    // fetch().then().then()
+    // 기존 데이터 세팅
+    setUsers([...sample, { id: num, name: '조자룡' }]); // ...sample 값이 4개라 { id: num, name: '조자룡' } <--- 값만 변경됨
+    setNum(num + 1);
   };
 
-  // 랜더링 시점 = 상태값 변경
   return (
     <div>
-      <h1>숫자 : {number}</h1>
-      <button onClick={add}>더하기</button>
-      <Sub />
+      <button onClick={download}>다운로드</button>
+      {users.map((u) => (
+        <h1>
+          {u.id},{u.name}
+        </h1>
+      ))}
     </div>
   );
 }
