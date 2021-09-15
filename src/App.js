bc6645d87bbcbde9edd142cee0c13ea347c6e57d
wsoh <wsoh@foodtechkorea.com>
2021-09-16 08:55:45 +0900

    #10. useEffect 완료

--- src/App.js
+++ src/App.js
@@ -1,38 +1,46 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react/cjs/react.development';
+import Third from './aa/Third';
 import './App.css';
 import Sub from './Sub';
+import { num } from './Sub';
 
 function App() {
-  console.log('App 실행됨');
-
-  const [num, setNum] = useState(5);
-
-  let sample = [
-    { id: 1, name: '홍길동' },
-    { id: 2, name: '임꺽정' },
-    { id: 3, name: '코스' },
-    { id: 4, name: '장보고' },
-  ];
-
-  // 다운로드 받음
-  // React안에 hooks 라이브러리
-  const [users, setUsers] = useState(sample); // 레퍼런스 변경되야 동작!!
+  const [data, setData] = useState(0);
+  const [search, setSearch] = useState(0);
 
   const download = () => {
-    // fetch().then().then()
-    // 기존 데이터 세팅
-    setUsers([...sample, { id: num, name: '조자룡' }]); // ...sample 값이 4개라 { id: num, name: '조자룡' } <--- 값만 변경됨
-    setNum(num + 1);
+    // 다운로드 받고 (통신)
+    let downloadData = 5; // 가정
+    setData(downloadData);
   };
+
+  // 실행시점
+  // (1) App() 그림이 최초 그려질때
+  // (2) 상태 변수가 변경될 때
+  // (3) 의존리스트 관리를 할수 있다.
+  useEffect(() => {
+    console.log('useEffect 실행됨');
+    download();
+  }, [search]);
 
   return (
     <div>
-      <button onClick={download}>다운로드</button>
-      {users.map((u) => (
-        <h1>
-          {u.id},{u.name}
-        </h1>
-      ))}
+      <button
+        onClick={() => {
+          setSearch(2);
+        }}
+      >
+        검색하기
+      </button>
+      <h1>데이터 : {data}</h1>
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+      >
+        더하기
+      </button>
     </div>
   );
 }
--- src/Sub.js
+++ src/Sub.js
@@ -1,15 +1,9 @@
-// rsc
 import React from 'react';
 
-// 다시 그리는것에 대한 함수 제공 !!!
-// 부모가 변경되면 자식도 재빌드
+let num = 10;
 
-const Sub = () => {
-  return (
-    <div>
-      <h1>Sub입니다.</h1>
-    </div>
-  );
-};
-
+function Sub() {
+  return <div></div>;
+}
+export { num };
 export default Sub;
