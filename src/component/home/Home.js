efb2b1fc521535b08469408b4810b5ba6845ac1e
wooseokoh <allsafe1206@gmail.com>
2021-09-20 13:07:59 +0900

    #16. styled-component 스타일 상속

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -3,9 +3,15 @@ import styled from 'styled-components';
 
 // 부모로 부터 받아온 어떤 데이터를 가지고 스타일링을 동적으로 할것이라면?
 
-let StyledDeleteButton = styled.button`
+
+const StyledDeleteButton = styled.button`
         color: ${(props) => props.user.username === 'ssar' ? 'blue' : 'red'};
-    `;
+`;
+
+const StyledAddButton = styled(StyledDeleteButton)`
+    color: ${(props) => props.user.username === 'ssar' ? 'blue' : 'red'};
+    background-color: green;
+`;
 
 const Home = (props) => {
     
@@ -15,7 +21,7 @@ const Home = (props) => {
     return (
         <div>
             <h1>홈</h1>
-            
+            <StyledAddButton user={user}>더하기</StyledAddButton>
             <StyledDeleteButton user={user} onClick={() => setBoards([])}>
                 전체삭제
             </StyledDeleteButton>
